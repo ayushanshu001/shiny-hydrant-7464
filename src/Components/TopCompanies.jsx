dbe55f99e550748d8026c9cf6dd1c4cfc6e8520c
paramjeetrawat265 <paramjeetrawat265@gmail.com>
2022-09-28 12:36:48 +0530

    Top Company Section

--- src/Components/TopCompanies.jsx
+++ src/Components/TopCompanies.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { TopCompanyData } from "../Data/TopCompanyData"
+import CardsCarousel from './CardsCarousel';
+// import "../Resources/Mnc"
 
 const TopCompanies = () => {
-  return (
-    <div>TopCompanies</div>
-  )
+
+    return (
+        <div>
+            <h1>Top Companies Hiring Now</h1>
+            <CardsCarousel CompanyData={TopCompanyData}/>
+        </div>
+    )
 }
 
 export default TopCompanies

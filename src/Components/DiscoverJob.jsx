3d96e807ae534ebcbeef837aa63e51329e353671
paramjeetrawat265 <paramjeetrawat265@gmail.com>
2022-09-28 21:49:57 +0530

    MultiGrid

---
+++ src/Components/DiscoverJob.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import Carousel from 'react-grid-carousel';
+import "./MultiGridCarousel.css";
+import { PopularRolesData } from '../Data/PopularRolesData';
+import { HiOutlineChevronRight } from "react-icons/hi"
+
+const Gallery = () => {
+  PopularRolesData.map((items) => {
+    console.log(items.ele)
+  })
+  return (
+    <div className="discoverMainContainer">
+      <Carousel cols={1} rows={1}>
+        {PopularRolesData.map((items, index) => (
+          <Carousel.Item key={index} >
+            <div className="discoverCards">
+              {items.ele.map((element, index) => (
+                <div className="discoverSingleCard" key={index}>
+                  <div className="discoverRoles">
+                    <p className="discoverRolesName">{element.name}</p>
+                    <div>
+                      <p className="discoverRolesJob">{element.jobs}</p>
+                      <p><HiOutlineChevronRight /></p>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </Carousel.Item>
+        ))
+        }
+      </Carousel>
+    </div>
+  )
+}
+export default Gallery
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Gallery from '../Components/DiscoverJob'
 import FeaturedCompanies from '../Components/FeaturedCompanies'
 import TopCompanies from '../Components/TopCompanies'
 
@@ -7,6 +8,7 @@ const Home = () => {
     <div>
         <TopCompanies/>
         <FeaturedCompanies/>
+        <Gallery/>
     </div>
   )
 }

2db572002e3303139b5e3a23c287aefe4dbccda0
paramjeetrawat265 <paramjeetrawat265@gmail.com>
2022-10-01 00:19:01 +0530

    single page about section

--- src/Components/SingleCompany.jsx
+++ src/Components/SingleCompany.jsx
@@ -1,30 +1,71 @@
-import axios from 'axios'
-import React, { useEffect } from 'react'
-import { useParams } from 'react-router-dom'
-import styled from 'styled-components'
+import axios from 'axios';
+import { Checkmark } from 'react-checkmark'
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import "../Styles/SingleCompany.css";
+
+import { AiFillStar } from 'react-icons/ai';
 
 const SingleCompany = () => {
 
-    let data={}
+    const [data, setData] = useState({})
     console.log(data)
-    const getCompany=()=>{
+    const getCompany = () => {
         return axios
-        .get(`https://chalo-api.herokuapp.com/Cognizant`)
-        .then(res=>{data=res.data})
-        .catch(e=>console.log(e))
-      }
-      
-      useEffect(()=>{
+            .get(`https://chalo-api.herokuapp.com/Cognizant`)
+            .then(res => { setData(res.data) })
+            .catch(e => console.log(e))
+    }
+
+    useEffect(() => {
         getCompany();
-      },[])
+    }, [])
 
-  return (
-    <SingleCompanyContainer>
-        <img src=""/>
-    </SingleCompanyContainer>
-  )
+    return (
+        <SingleCompanyContainer>
+            {data &&
+                <div className="singleMainContainer">
+                    <div className="bannerContainer">
+                        <img src={data.banner} className="banner" />
+                        <div className="manage">
+                            <p>Managed by employer</p>
+                            <Checkmark size="small"></Checkmark>
+                        </div>
+                    </div>
+                    <div className="singleContent">
+                        <div className="singleCompanyIntro">
+                            <div className="singleCompanyLogo">
+                                <img src={data.image} alt="companyLogo"></img>
+                            </div>
+                            <div className="singleCompanyIntroRight">
+                                <div className="singleCompanyHead">
+                                    <p className="singleCompanyTitle">{data.name}</p>
+                                    <p className="singleCompanyRating">{data.rating}</p>
+                                    <AiFillStar style={{ color: "orange" }} />
+                                    <p className="singleCompanyReview">{data.review}</p>
+                                </div>
+                                <div className="singleCompanySubhead">
+                                    <p className="singleCompanyTagline">{data.subHead}</p>
+                                    <div className="singleCompanyFollow">
+                                        <p>{"116.4k followers" || data.followers}</p>
+                                        <a href=""><button>+Follow</button></a>
+                                    </div>
+                                </div>
+                                <div className="singleCompanySection">
+                                    {data.section?data.section.map((ele,index) => (
+                                        <button key={index}>{ele}</button>
+                                    )):""}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            }
+        </SingleCompanyContainer>
+    )
 }
-const SingleCompanyContainer=styled.div`
-
+const SingleCompanyContainer = styled.div`  
+padding:0px;
+margin:0px;
 `;
 export default SingleCompany

aa60aa020c2eacf3a12ae6b2ba7423b56031e19a
paramjeetrawat265 <paramjeetrawat265@gmail.com>
2022-09-30 10:11:49 +0530

    changes

---
+++ src/Pages/FeaturedCompanies.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FeaturedCompanies = () => {
+  return (
+    <div>
+        <h1>Featured companies actively hiring</h1>
+    </div>
+  )
+}
+
+export default FeaturedCompanies

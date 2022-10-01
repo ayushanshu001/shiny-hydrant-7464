a5099f45f1adc2578ee91e8a7199552ad28d1710
Ayush Anshu <ayushanshu56@gmail.com>
2022-10-01 20:34:24 +0530

    updates till 8:30PM Saturday

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react"
 import './App.css'
 import './App.css'
+import Footerr from "./Components/Footerr";
 import Navbar from "./Components/Navbar";
 import AllRout from "./Routing/AllRout";
 
@@ -8,7 +9,9 @@ import AllRout from "./Routing/AllRout";
 function App() {
   return (
     <div className="App">
-      
+      <Navbar/>
+      <AllRout/>
+      <Footerr/>
     </div >
   )
 }

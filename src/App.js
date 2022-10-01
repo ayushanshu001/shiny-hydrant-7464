7cade28232f1124772b87cca64aade547380d29d
Ayush Anshu <ayushanshu56@gmail.com>
2022-10-01 21:04:10 +0530

    Fresh updates till 9 PM Saturday

--- src/App.js
+++ src/App.js
@@ -1,27 +1,8 @@
-
-
-import './App.css';
-import Prepareforyour from'./Components_Avi/Prepareforyour'
-import Accelerateyourjob   from './Components_Avi/Accelerateyourjob';
-import Growyour from './Components_Avi/Growyour';
-import Standout from './Components_Avi/Standout';
-import Comp_Side from './Components_Avi/Comp_Side';
-
 import React from "react"
 import './App.css'
-import './App.css'
-import Footerr from "./Components/Footerr";
 import Navbar from "./Components/Navbar";
 import AllRout from "./Routing/AllRout";
-import './App.css' 
-import Navbar from './Components/Navbar';
-import SingleCompany from "./Components/SingleCompany";
-import AllRout from './Routing/AllRout';
-
-import React from "react"
 import './App.css'
-import './App.css'
-import SingleCompanyPage from "./Pages/SingleCompanyPage";
 
 
 function App() {
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,11 +1,7 @@
 
 import style from '../Styles/navbar.module.css'
-import React from 'react'
 import {Link} from 'react-router-dom'
 import React, { useState } from 'react'
-import style from '../Styles/Navbar.module.css'
-
-import { Link } from 'react-router-dom'
 
 const Navbar = () => {
     return (

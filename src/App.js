1be2e8932291f9b09efaf28f35f44aaba5ee873e
Ayush Anshu <ayushanshu56@gmail.com>
2022-10-01 05:38:42 +0530

    Updates till 5 AM Saturday-Day-3

--- src/App.js
+++ src/App.js
@@ -7,11 +7,18 @@ import Navbar from './Component/Navbar';
 import Footerr from './Component/Footerr';
 import AllRout from './Routing/AllRout';
 import DreamJob from './Component/DreamJob';
+import {ChakraProvider} from '@chakra-ui/react'
+import All from './Backup file/Balaji/Routes/All';
+import Sponsoredcompanies from './Backup file/Balaji/Components/Sponsoredcompanies'
 
 function App() {
   return (
     <div className="App">
-      
+      {/* <Navbar/> */}
+      {/* <AllRout/> */}
+      {/* <DreamJob/> */}
+      {/* <Navbar/>
+      <AllRout/> */}
    </div>
   )
 }
---
+++ src/Backup file/Balaji/Routes/BFSI.jsx
@@ -0,0 +1,329 @@
+import React from "react";
+import style from "../Components/Allimg.module.css";
+import { Grid, GridItem } from "@chakra-ui/react";
+
+const BSFI = () => {
+  return (
+    <div>
+
+<Grid templateColumns="repeat(5, 1fr)" gap={6}  mt="10px">
+        <GridItem className={style.class_Griditems} >
+          <div>
+            <img
+              src="https://img.naukimg.com/logo_images/groups/v1/3020858.gif"
+              alt="Finastra"
+              width={55}
+              className={style.imgclass}
+            />
+            <h1>
+              <b>Finastra</b>
+            </h1>
+            <div className={style.flexitems}>
+              <img
+                src="https://static.naukimg.com/s/7/0/assets/images/node_modules/@naukri-ui-dev/premiumstandardads/component/assets/star.625a2323.svg"
+                alt="Rating"
+              />
+              <p>
+                <b>4.5</b>
+              </p>
+              <p>
+                <b>34 reviews</b>
+              </p>
+            </div>
+            <div className={style.tags}>
+            <p className={style.prod_type}>Fintech</p>
+            <p className={style.prod_type}>Foreign MNC</p>
+            <p className={style.prod_type}>Service</p>
+            </div>
+          </div>
+        </GridItem>
+
+        <GridItem className={style.class_Griditems} mb="20px">
+          <div>
+            <img
+              src="https://img.naukimg.com/logo_images/groups/v1/374224.gif"
+              alt="Invesco"
+              width={55}
+              className={style.imgclass}
+            />
+            <h1>
+              <b>Invesco</b>
+            </h1>
+            <div className={style.flexitems}>
+              <img
+                src="https://static.naukimg.com/s/7/0/assets/images/node_modules/@naukri-ui-dev/premiumstandardads/component/assets/star.625a2323.svg"
+                alt="Rating"
+              />
+              <p>
+                <b>3.5</b>
+              </p>
+              <p>
+                <b>150 reviews</b>
+              </p>
+              
+            </div>
+            <div className={style.tags}>
+            <p className={style.prod_type}>Forbes 2000</p>
+            <p className={style.prod_type}>Foreign MNC</p>
+            
+            </div>
+          </div>
+        </GridItem>
+        <GridItem className={style.class_Griditems}>
+          <div>
+            <img
+              src="https://img.naukimg.com/logo_images/groups/v1/4655761.gif"
+              alt="Wellfargo"
+              width={55}
+              className={style.imgclass}
+            />
+            <h1>
+              <b>State Street</b>
+            </h1>
+            <div className={style.flexitems}>
+              <img
+                src="https://static.naukimg.com/s/7/0/assets/images/node_modules/@naukri-ui-dev/premiumstandardads/component/assets/star.625a2323.svg"
+                alt="Rating"
+              />
+              <p>
+                <b>4.3</b>
+              </p>
+              <p>
+                <b>224 reviews</b>
+              </p>
+            </div>
+            <div className={style.tags}>
+            <p className={style.prod_type}>B2B</p>
+            <p className={style.prod_type}>Foreign MNC</p>
+            <p className={style.prod_type}>IT Product</p>
+            </div>
+          </div>
+        </GridItem>
+        <GridItem className={style.class_Griditems}>
+          <div>
+            <img
+              src="https://img.naukimg.com/logo_images/groups/v1/3584.gif"
+              alt="ICICI"
+              width={55}
+              className={style.imgclass}
+            />
+            <h1>
+              <b>ICICI Prudentials</b>
+            </h1>
+            <div className={style.flexitems}>
+              <img
+                src="https://static.naukimg.com/s/7/0/assets/images/node_modules/@naukri-ui-dev/premiumstandardads/component/assets/star.625a2323.svg"
+                alt="Rating"
+              />
+              <p>
+                <b>4.7</b>
+              </p>
+              <p>
+                <b>898k+ reviews</b>
+              </p>
+            </div>
+            <div className={style.tags}>
+            <p className={style.prod_type}>B2B</p>
+            <p className={style.prod_type}>Indian </p>
+            <p className={style.prod_type}>Insurance</p>
+            </div>
+          </div>
+        </GridItem>
+        <GridItem className={style.class_Griditems}>
+          <div>
+            <img
+              src="https://img.naukimg.com/logo_images/groups/v1/1371070.gif"
+              alt="Stanplus"
+              width={55}
+              className={style.imgclass}
+            />
+            <h1>
+              <b>Stanplus Tech...</b>
+            </h1>
+            <div className={style.flexitems}>
+              <img
+                src="https://static.naukimg.com/s/7/0/assets/images/node_modules/@naukri-ui-dev/premiumstandardads/component/assets/star.625a2323.svg"
+                alt="Rating"
+              />
+              <p>
+                <b>4.5</b>
+              </p>
+              <p>
+                <b>34 reviews</b>
+              </p>
+            </div>
+            <div className={style.tags}>
+            <p className={style.prod_type}>B2A</p>
+            <p className={style.prod_type}>Foreign MNC</p>
+            <p className={style.prod_type}>Software Product</p>
+            </div>
+          </div>
+        </GridItem>
+      </Grid>
+     <div>
+
+     </div>
+
+
+
+    <div  className={style.classGridItems}>
+    <Grid templateColumns="repeat(5, 1fr)" gap={6}>
+        <GridItem className={style.class_Griditems}>
+          <div>
+            <img
+              src="https://img.naukimg.com/logo_images/groups/v1/134834.gif"
+              alt="Synchrony"
+              width={55}
+              className={style.imgclass}
+            />
+            <h1>
+              <b>Synchrony</b>
+            </h1>
+            <div className={style.flexitems}>
+                           
+                <b>4.5</b>
+             
+              <p>
+                <b>124 reviews</b>
+              </p>
+            </div>
+            <div className={style.tags}>
+            <p className={style.prod_type}>B2C</p>
+            <p className={style.prod_type}>Foreign MNC</p>
+            <p className={style.prod_type}>Tech Product</p>
+            </div>
+          </div>
+        </GridItem>
+
+<GridItem className={style.class_Griditems}>
+          <div>
+            <img
+              src="https://img.naukimg.com/logo_images/groups/v1/86414.gif"
+              alt="Fulltron"
+              width={55}
+              className={style.imgclass}
+            />
+            <h1>
+              <b>Fulltron</b>
+            </h1>
+            <div className={style.flexitems}>
+              <img
+                src="https://static.naukimg.com/s/7/0/assets/images/node_modules/@naukri-ui-dev/premiumstandardads/component/assets/star.625a2323.svg"
+                alt="Rating"
+              />
+              <p>
+                <b>4</b>
+              </p>
+              <p>
+                <b>344k reviews</b>
+              </p>
+              
+            </div>
+            <div className={style.tags}>
+            <p className={style.prod_type}>NBFC</p>
+            <p className={style.prod_type}>Corparate MNC</p>
+            
+            </div>
+          </div>
+        </GridItem>
+        <GridItem className={style.class_Griditems}>
+          <div>
+            <img
+              src="https://img.naukimg.com/logo_images/groups/v1/2990794.gif"
+              alt="Wellfargo"
+              width={55}
+              className={style.imgclass}
+            />
+            <h1>
+              <b>Apisero</b>
+            </h1>
+            <div className={style.flexitems}>
+              <img
+                src="https://static.naukimg.com/s/7/0/assets/images/node_modules/@naukri-ui-dev/premiumstandardads/component/assets/star.625a2323.svg"
+                alt="Rating"
+              />
+              <p>
+                <b>4.2</b>
+              </p>
+              <p>
+                <b>123 reviews</b>
+              </p>
+            </div>
+            <div className={style.tags}>
+            <p className={style.prod_type}>IT Services</p>
+            <p className={style.prod_type}>B2B</p>
+            <p className={style.prod_type}>Consulting </p>
+            </div>
+          </div>
+        </GridItem>
+        <GridItem className={style.class_Griditems}>
+          <div>
+            <img
+              src="https://img.naukimg.com/logo_images/groups/v1/2082772.gif"
+              alt="Software AG"
+              width={55}
+              className={style.imgclass}
+            />
+            <h1>
+              <b>Software AG</b>
+            </h1>
+            <div className={style.flexitems}>
+              <img
+                src="https://static.naukimg.com/s/7/0/assets/images/node_modules/@naukri-ui-dev/premiumstandardads/component/assets/star.625a2323.svg"
+                alt="Rating"
+              />
+              <p>
+                <b>4.5</b>
+              </p>
+              <p>
+                <b>34 reviews</b>
+              </p>
+            </div>
+            <div className={style.tags}>
+            <p className={style.prod_type}>B2B</p>
+            <p className={style.prod_type}>Foreign MNC</p>
+            <p className={style.prod_type}>Software Product</p>
+            </div>
+          </div>
+        </GridItem>
+        <GridItem className={style.class_Griditems}>
+          <div>
+            <img
+              src="https://img.naukimg.com/logo_images/groups/v1/4626219.gif"
+              alt="Stanplus"
+              width={55}
+              className={style.imgclass}
+            />
+            <h1>
+              <b>Stanplus Tech...</b>
+            </h1>
+            <div className={style.flexitems}>
+              <img
+                src="https://static.naukimg.com/s/7/0/assets/images/node_modules/@naukri-ui-dev/premiumstandardads/component/assets/star.625a2323.svg"
+                alt="Rating"
+              />
+              <p>
+                <b>4.5</b>
+              </p>
+              <p>
+                <b>34 reviews</b>
+              </p>
+            </div>
+            <div className={style.tags}>
+            <p className={style.prod_type}>B2B</p>
+            <p className={style.prod_type}>Foreign MNC</p>
+            <p className={style.prod_type}>Software Product</p>
+            </div>
+          </div>
+        </GridItem>
+
+
+      </Grid>
+    </div>
+
+
+    </div>
+  );
+};
+
+export default BSFI;
